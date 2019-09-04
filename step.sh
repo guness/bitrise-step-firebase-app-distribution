6cd2edcf6244be7317f818270b65617e1b524cb4
Sinan Gunes <gunes.sinan@gmail.com>
2019-09-04 15:51:02 +0300

    Firebase App Distribution started

--- step.sh
+++ step.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -ex
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+#echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
 
 #
 # --- Export Environment Variables for other Steps:
 # You can export Environment Variables for other Steps with
 #  envman, which is automatically installed by `bitrise setup`.
 # A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
+#envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
 # Envman can handle piped inputs, which is useful if the text you want to
 # share is complex and you don't want to deal with proper bash escaping:
 #  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
@@ -20,3 +20,20 @@ envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
 # The exit code of your Step is very important. If you return
 #  with a 0 exit code `bitrise` will register your Step as "successful".
 # Any non zero exit code will be registered as "failed" by `bitrise`.
+
+
+# Install Firebase
+npm install -g firebase-tools
+
+# Enable Firebase App Distribution
+firebase --open-sesame appdistribution
+
+# Export Firebase Token
+export FIREBASE_TOKEN="${firebase_token}"
+
+# Deploy
+firebase appdistribution:distribute "${app_path}" \
+--app "${app}" \
+--release-notes "${release_notes}" \
+--testers "${testers}" \
+--groups "${groups}"
