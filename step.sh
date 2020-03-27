31491fd7db4ab1d1ecd70da5fb674fc1e281e16f
Sinan Gunes <gunes.sinan@gmail.com>
2020-03-28 00:25:29 +0300

    should fix #17, escaping release notes

--- step.sh
+++ step.sh
@@ -55,6 +55,12 @@ function validate_required_input {
     fi
 }
 
+function escape {
+    token=$1
+    quoted=$(echo "${token}" | sed -e 's/\"/\\"/g' )
+    echo "${quoted}"
+}
+
 function validate_required_input_with_options {
     key=$1
     value=$2
@@ -160,28 +166,28 @@ fi
 # Deploy
 echo_info "Deploying build to Firebase"
 
-submit_cmd="firebase appdistribution:distribute '${app_path}'"
+submit_cmd="firebase appdistribution:distribute \"${app_path}\""
 submit_cmd="$submit_cmd --app \"${app}\""
 
 ## Optional params
 if [ -n "${release_notes}" ] ; then
-    submit_cmd="$submit_cmd --release-notes ${release_notes}"
+    submit_cmd="$submit_cmd --release-notes \"$(escape "$release_notes")\""
 fi
 
 if [ -n "${release_notes_file}" ] && [ -f "${release_notes_file}" ] ; then
-    submit_cmd="$submit_cmd --release-notes-file '${release_notes_file}'"
+    submit_cmd="$submit_cmd --release-notes-file \"${release_notes_file}\""
 fi
 
 if [ -n "${testers}" ] ; then
-    submit_cmd="$submit_cmd --testers '${testers}'"
+    submit_cmd="$submit_cmd --testers \"${testers}\""
 fi
 
 if [ -n "${groups}" ] ; then
-    submit_cmd="$submit_cmd --groups '${groups}'"
+    submit_cmd="$submit_cmd --groups \"${groups}\""
 fi
 
 if [ -n "${flags}" ] ; then
-    submit_cmd="$submit_cmd '${flags}'"
+    submit_cmd="$submit_cmd \"${flags}\""
 fi
 
 if [ "${is_debug}" = true ] ; then
@@ -191,7 +197,7 @@ fi
 echo_details "$submit_cmd"
 echo
 
-${submit_cmd}
+eval "${submit_cmd}"
 
 if [ $? -eq 0 ] ; then
     echo_done "Success"
