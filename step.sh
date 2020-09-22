acd335aaab3ba63f367e4cf9a5938180bc588acb
Sinan Gunes <gunes.sinan@gmail.com>
2020-09-23 00:03:56 +0300

    Download Service Credentials file improvements

--- step.sh
+++ step.sh
@@ -133,7 +133,7 @@ if [ -z "${firebase_token}" ] ; then
         if [[ $service_credentials_file == http* ]]; then
           echo_info "Service Credentials File is a remote url, downloading it ..."
           curl $service_credentials_file --output credentials.json
-          export service_credentials_file=$(pwd)/credentials.json
+          service_credentials_file=$(pwd)/credentials.json
           echo_info "Downloaded Service Credentials File to path: ${service_credentials_file}"
         else
           echo_fail "Service Credentials File defined but does not exist at path: ${service_credentials_file}"
